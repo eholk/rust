65d0a44f8fd907a37321c2ef58cf7c328d1030de
Eric Holk <ericholk@microsoft.com>
2021-10-01 14:49:32 -0700

    Bless another test

--- src/test/ui/async-await/async-fn-nonsend.rs
+++ src/test/ui/async-await/async-fn-nonsend.rs
@@ -1,28 +1,13 @@
 // edition:2018
 // compile-flags: --crate-type lib
 
-use std::{cell::RefCell, fmt::Debug, rc::Rc};
-
-fn non_sync() -> impl Debug {
-    RefCell::new(())
-}
+use std::{fmt::Debug, rc::Rc};
 
 fn non_send() -> impl Debug {
     Rc::new(())
 }
 
-fn take_ref<T>(_: &T) {}
-
 async fn fut() {}
-
-async fn fut_arg<T>(_: T) {}
-
-async fn local_dropped_before_await() {
-    // FIXME: it'd be nice for this to be allowed in a `Send` `async fn`
-    let x = non_send();
-    drop(x);
-    fut().await;
-}
 
 async fn non_send_temporary_in_match() {
     // We could theoretically make this work as well (produce a `Send` future)
@@ -35,21 +20,10 @@ async fn non_send_temporary_in_match() {
     }
 }
 
-async fn non_sync_with_method_call() {
-    // FIXME: it'd be nice for this to work.
-    let f: &mut std::fmt::Formatter = panic!();
-    if non_sync().fmt(f).unwrap() == () {
-        fut().await;
-    }
-}
-
 fn assert_send(_: impl Send) {}
 
 pub fn pass_assert() {
-    assert_send(local_dropped_before_await());
-    //~^ ERROR future cannot be sent between threads safely
     assert_send(non_send_temporary_in_match());
     //~^ ERROR future cannot be sent between threads safely
-    assert_send(non_sync_with_method_call());
-    //~^ ERROR future cannot be sent between threads safely
+    //~^^ ERROR future cannot be sent between threads safely
 }
