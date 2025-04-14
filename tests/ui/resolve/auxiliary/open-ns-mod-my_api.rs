a68d466cb9d6c704c05fca91b312c5416ddc81ee
b-naber <b_naber@gmx.de>
2025-04-14 18:49:05 +0000

    add tests

---
+++ tests/ui/resolve/auxiliary/open-ns-mod-my_api.rs
@@ -0,0 +1,9 @@
+pub mod utils {
+    pub fn utils_helper() {
+        println!("helper function");
+    }
+}
+
+pub fn root_function() -> String {
+    "my_api root!".to_string()
+}
---
+++ tests/ui/resolve/open-ns-3.rs
@@ -0,0 +1,8 @@
+//@ aux-crate: my_api=open-ns-mod-my_api.rs
+//@ aux-crate: my_api::utils=open-ns-my_api_utils.rs
+
+fn test() {
+    let _ = my_api::root_function();
+    let _ = my_api::utils::root_helper();
+    let _ = my_api::utils::utils_helper();
+}
